51ab1500bc73177cc5b7cebfe7fbdfbac9d5dbd0
nare162rk <yoursnnr@gmail.com>
2026-02-06 10:51:58 +0530

    workflow added

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. Import it
+import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 2. Add it here
+    tailwindcss(),
   ],
+  base: '/', // IMPORTANT for Firebase Hosting
 })
